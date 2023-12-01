ffc725159b8606dad82e5d9453a1e8c9d607ac7a
octehren <ehren.dev.mail@gmail.com>
2023-12-01 03:38:27 +0100

    added logic to handle version ranges

--- lib/gemfile_info.rb
+++ lib/gemfile_info.rb
@@ -15,14 +15,26 @@ module Lapidario
       gem_name_item.sub("gem ", "").gsub("'", "")
     end
 
-    def self.get_version_field(gemfile_line_items)
-      version_number_and_sign = gemfile_line_items
-                                .map { |item| item.gsub('"', "").gsub("'", "").gsub(" ", "") } # can be something like [" '~> 6.3'"]
-                                .find { |item| Lapidario::Helper.version_fragment? item }
-      return ["", ""] unless version_number_and_sign
+    # detects version field, removes it from array and returns it
+    def self.pop_version_field!(gemfile_line_items)
+      version_index = 0
+      version_number_and_sign = []
+      gemfile_line_items
+        .map { |item| item.gsub('"', "").gsub("'", "").gsub(" ", "") } # can be something like [" '~> 6.3'"]
+        .each_with_index do |item, index| 
+          if Lapidario::Helper.version_fragment? item
+            version_number_and_sign = item
+            version_index = index
+            break # will break at first index, so if there is an upper range index later this won't affect results
+          end
+        end
 
+      return [] if version_number_and_sign.empty?
+      
       version_number = version_number_and_sign.gsub(/(~>|>=|<=|>|<)/, "")
       version_sign = version_number_and_sign.gsub(/[^~>=<]/, "")
+      
+      gemfile_line_items.delete_at(version_index)
       [version_number, version_sign]
     end
 
@@ -31,12 +43,16 @@ module Lapidario
       gemfile_line_items = gemfile_line.split(",")
       # first element will always be gem name, so return and remove it with shift
       gem_info[:name] = Lapidario::GemfileInfo.get_gem_name gemfile_line_items.shift
-      version_number_and_sign = Lapidario::GemfileInfo.get_version_field(gemfile_line_items)
-      gem_info[:current_version] = version_number_and_sign[0]
-      gem_info[:version_sign] = version_number_and_sign[1]
-      gemfile_line_items.delete(version_number_and_sign)
+      # second might not be the version number, so find the version, store it separetly and remove it from the rest of the line
+      version_number_and_sign = Lapidario::GemfileInfo.pop_version_field!(gemfile_line_items)
+      # to_s will make a nil value an empty string
+      gem_info[:current_version] = version_number_and_sign[0].to_s
+      gem_info[:version_sign] = version_number_and_sign[1].to_s
+      # in case version is ranged, run pop_version_field! again, just removing the field and appending it to the beginning of extra info, immediately after lower range.
+      version_upper_range = Lapidario::GemfileInfo.pop_version_field!(gemfile_line_items)
+      gemfile_line_items.unshift("'#{version_upper_range[1]} #{version_upper_range[0]}'") unless version_upper_range.empty?
       # all that's left is extra_info
-      gem_info[:extra_info] = gemfile_line_items.join(", ")
+      gem_info[:extra_info] = gemfile_line_items.compact.join(", ").split(",").join(", ").gsub(/\s+/, ' ').strip
       gem_info
     end
 
@@ -49,7 +65,7 @@ module Lapidario
     end
 
     def self.build_gemfile_line(gi) # gi = gem_info
-      "gem '#{gi[:name]}', '#{gi[:version_sign]} #{gi[:current_version]}', #{gi[:extra_info].split(",").join(", ").gsub(/\s+/, ' ').strip}"
+      "gem '#{gi[:name]}', '#{gi[:version_sign]} #{gi[:current_version]}', #{gi[:extra_info]}"
     end
   end
 end
