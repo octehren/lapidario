2350180a3a749fec2d86f8d59afdf2bbba8814b5
octehren <ehren.dev.mail@gmail.com>
2023-12-01 20:31:14 +0100

    added function to rebuild gemfile

--- lib/lapidario.rb
+++ lib/lapidario.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require "pry"
 require_relative "lapidario/version"
 require_relative "helper"
 require_relative "lockfile_info"
@@ -12,11 +11,8 @@ module Lapidario
     # gemfile_lock = Helper.get_file_as_array_of_lines(gemfile_lock_path)
     # GemfileLockInfo.new(gemfile_lock)
   end
-  # project_path_hash = {}
-  # project_path_hash[:gemfile_path] = "/Users/Otavio_Ehrenberger/Documents/DevelopingLibraries/Ruby/lapidario/spec/fixtures/gemfile_samples/Gemfile.lapidario_v01"
-  # project_path_hash[:lockfile_path] = "/Users/Otavio_Ehrenberger/Documents/DevelopingLibraries/Ruby/lapidario/spec/fixtures/lockfile_samples/Gemfile.lock.lapidario_v01"
-  # Lapidario.hardcode_lockfile_versions_into_gemfile(project_path_hash)
-  def self.hardcode_lockfile_versions_into_gemfile(project_path_hash)
+
+  def self.get_gemfile_and_lockfile_info(project_path_hash)
     project_path = project_path_hash[:project_path]
     if (project_path)
       gemfile_path, lockfile_path = Lapidario::Helper.format_path(project_path, false), Lapidario::Helper.format_path(project_path, true)
@@ -26,11 +22,31 @@ module Lapidario
     
     gemfile_info = Lapidario::GemfileInfo.new(Lapidario::Helper.get_file_as_array_of_lines(gemfile_path))
     lockfile_info = Lapidario::LockfileInfo.new(Lapidario::Helper.get_file_as_array_of_lines(lockfile_path))
-    binding.pry
+    [gemfile_info, lockfile_info]
+  end
+
+  def self.hardcode_lockfile_versions_into_gemfile_info(gemfile_info, lockfile_info)
+    new_gemfile_info = gemfile_info.gemfile_lines_info.clone
+    lockfile_primary_gems = lockfile_info.primary_gems
+    # replace versions in gemfile with ones on lockfile
+    new_gemfile_info.each do |gem_info|
+      lock_version = lockfile_primary_gems[gem_info[:name]]
+      gem_info[:current_version] = lock_version if lock_version
+    end
+    new_gemfile_info
+  end
+
+  def self.build_new_gemfile(new_gemfile_info, original_gemfile_lines)
+    new_gemfile = original_gemfile_lines.clone
+    new_gemfile_info.each do |info_item|
+      new_line = Lapidario::GemfileInfo.build_gemfile_line(info_item)
+      new_gemfile[info_item[:line_number]] = new_line
+    end
+    new_gemfile
   end
 end
 
 project_path_hash = {}
 project_path_hash[:gemfile_path] = "/Users/Otavio_Ehrenberger/Documents/DevelopingLibraries/Ruby/lapidario/spec/fixtures/gemfile_samples/Gemfile.lapidario_v01"
 project_path_hash[:lockfile_path] = "/Users/Otavio_Ehrenberger/Documents/DevelopingLibraries/Ruby/lapidario/spec/fixtures/lockfile_samples/Gemfile.lock.lapidario_v01"
-Lapidario.hardcode_lockfile_versions_into_gemfile(project_path_hash)
+Lapidario.hardcode_lockfile_versions_into_gemfile_info(project_path_hash)
