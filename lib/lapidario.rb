7d1fb4f9369d8a547d7bd584e18029f1f4ae95a1
octehren <ehren.dev.mail@gmail.com>
2023-12-05 07:36:06 +0100

    refactors & comments

--- lib/lapidario.rb
+++ lib/lapidario.rb
@@ -7,6 +7,12 @@ require_relative "gemfile_info"
 module Lapidario
   class Error < StandardError; end
 
+  ######################
+  ## shared processes ## ex: steps for specific processes, used more than once
+  ######################
+
+  # first step: build gemfile and lockfile info instances
+  # input: project_path_hash; has either :project_path key or both :gemfile_path and :lockfile_path; returns GemfileInfo and LockfileInfo instances
   def self.get_gemfile_and_lockfile_info(project_path_hash)
     project_path = project_path_hash[:project_path]
     if (project_path && !project_path.empty?)
@@ -22,17 +28,7 @@ module Lapidario
     [gemfile_info, lockfile_info]
   end
 
-  def self.hardcode_lockfile_versions_into_gemfile_info(gemfile_info, lockfile_info)
-    new_gemfile_info = gemfile_info.gemfile_lines_info.clone
-    lockfile_primary_gems = lockfile_info.primary_gems
-    # replace versions in gemfile with ones on lockfile
-    new_gemfile_info.each do |gem_info|
-      lock_version = lockfile_primary_gems[gem_info[:name]]
-      gem_info[:current_version] = lock_version if lock_version
-    end
-    new_gemfile_info
-  end
-
+  # semi-final step: build new temporary gemfile, replacing changed lines in the original; intermediary object will be altered by one of the methods at the bottom of the page
   def self.build_new_gemfile(new_gemfile_info, original_gemfile_lines)
     new_gemfile = original_gemfile_lines.clone
     new_gemfile_info.each do |info_item|
@@ -42,6 +38,7 @@ module Lapidario
     new_gemfile
   end
 
+  # final step: save new, backup old
   def self.save_gemfiles(save_path, new_gemfile, original_gemfile)
     # save original gemfile as backup
     original_save_path = Lapidario::Helper.format_path(save_path) + ".original"
@@ -49,5 +46,22 @@ module Lapidario
     # overwrite current Gemfile with new content
     new_save_path = Lapidario::Helper.format_path(save_path)
     Lapidario::Helper.save_file(new_save_path, new_gemfile.join("\n"))
+  end
+
+  ######################
+  # specific processes # ex: specific functionality achieved through composition of shared processes
+  ######################
+
+  # loops through lockfile info, collecting versions
+  # hard-codes these versions on gemfile, as they might differ from the lockfile's
+  def self.hardcode_lockfile_versions_into_gemfile_info(gemfile_info, lockfile_info)
+    new_gemfile_info = gemfile_info.gemfile_lines_info.clone
+    lockfile_primary_gems = lockfile_info.primary_gems
+    # replace versions in gemfile with ones on lockfile
+    new_gemfile_info.each do |gem_info|
+      lock_version = lockfile_primary_gems[gem_info[:name]]
+      gem_info[:current_version] = lock_version if lock_version
+    end
+    new_gemfile_info
   end
 end
