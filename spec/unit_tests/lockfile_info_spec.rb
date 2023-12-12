2f2bf3caf1041d6a4b615278af8ae4e7b3dc8908
octehren <ehren.dev.mail@gmail.com>
2023-12-12 16:06:20 +0100

    added fixture & adjusted lockfile tests

--- spec/unit_tests/lockfile_info_spec.rb
+++ spec/unit_tests/lockfile_info_spec.rb
@@ -3,16 +3,15 @@
 require_relative "../spec_helper"
 
 SIMPLE_LOCKFILE_PATH = "simplified"
-
+GIT_GEMS_LOCKFILE_PATH = "git_simplified"
 RSpec.describe Lapidario::LockfileInfo do
   describe "using simple lockfile" do
     let(:lockfile_as_array_of_strings) { Lapidario::Helper.get_file_as_array_of_lines(get_lockfile_path(SIMPLE_LOCKFILE_PATH)) }
     let(:lockfile_info) { described_class.new(lockfile_as_array_of_strings) }
 
     describe '#initialize' do
-      it 'initializes with Rubygems gems and empty git gems' do
+      it 'initializes with Rubygems gems' do
         expect(lockfile_info.instance_variable_get(:@rubygems_gems)).to eq({"ast"=>"2.4.2", "coderay"=>"1.1.3", "diff-lcs"=>"1.5.0", "json"=>"2.6.3", "language_server-protocol"=>"3.17.0.3", "rubocop-ast"=>"1.30.0", "ruby-progressbar"=>"1.13.0", "unicode-display_width"=>"2.5.0"})
-        expect(lockfile_info.instance_variable_get(:@git_gems)).to eq([])
       end
     end
 
@@ -33,8 +32,8 @@ RSpec.describe Lapidario::LockfileInfo do
     end
 
     describe '#git_gems?' do
-      it 'complete later' do
-        TODO
+      it 'returns false if there are no git gems' do
+        expect(lockfile_info.git_gems?).to eq(false)
       end
     end
 
@@ -55,8 +54,55 @@ RSpec.describe Lapidario::LockfileInfo do
     end
 
     describe '.get_git_gems_from_gemfile_lock' do
-      it 'puts TODO message' do
-        expect { described_class.get_git_gems_from_gemfile_lock([]) }.to output(/TODO/).to_stdout
+      it 'detects a GIT gem and stores its information' do
+        gemfile_lock_as_strings = %q(
+          GIT
+            remote: https://github.com/ClearlyClaire/webpush.git
+            revision: f14a4d52e201128b1b00245d11b6de80d6cfdcd9
+            ref: f14a4d52e201128b1b00245d11b6de80d6cfdcd9
+            specs:
+              webpush (0.3.8)
+                hkdf (~> 0.2)
+                jwt (~> 2.0)
+                
+          ).split("\n")
+        expect { described_class.get_git_gems_from_gemfile_lock(lockfile_as_array_of_strings) }.to output([{"webpush" => "0.3.8, git: 'https://github.com/ClearlyClaire/webpush.git'"}]).to_stdout
+      end
+    end
+  end
+
+  describe "using lockfile with both rubygems gems and git gems" do
+    let(:lockfile_as_array_of_strings) { Lapidario::Helper.get_file_as_array_of_lines(get_lockfile_path(GIT_GEMS_LOCKFILE_PATH)) }
+    let(:lockfile_info) { described_class.new(lockfile_as_array_of_strings) }
+    describe '#git_gems?' do
+      it 'returns true if there are git gems' do
+        expect(lockfile_info.git_gems?).to eq(true)
+      end
+    end
+
+    describe '.get_rubygems_from_gemfile_lock' do
+      it 'returns a hash of rubygems gems' do
+
+        result = described_class.get_rubygems_from_gemfile_lock(lockfile_as_array_of_strings)
+
+        expect(result).to eq('ast' => '2.4.2', 'rubocop' => '1.57.2', 'rspec' => '3.12.0',)
+      end
+    end
+
+    describe '.get_git_gems_from_gemfile_lock' do
+      it 'detects a GIT gem and stores its information' do
+        gemfile_lock_as_strings = %q(
+          GIT
+            remote: https://github.com/ClearlyClaire/webpush.git
+            revision: f14a4d52e201128b1b00245d11b6de80d6cfdcd9
+            ref: f14a4d52e201128b1b00245d11b6de80d6cfdcd9
+            specs:
+              webpush (0.3.8)
+                hkdf (~> 0.2)
+                jwt (~> 2.0)
+                
+          ).split("\n")
+        expect(described_class.get_git_gems_from_gemfile_lock(lockfile_as_array_of_strings)).to eq("webpush" => "'0.3.8', git: 'https://github.com/ClearlyClaire/webpush.git'")
       end
     end
   end
